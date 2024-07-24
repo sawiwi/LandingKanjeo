7121e2e7974db9b959df112447f30fbc246f8e8e
Fabian Salas <fabian.salas.astete@gmail.com>
2024-07-24 01:13:45 -0400

    feat:fix size

--- src/components/howUse/HowUse.jsx
+++ src/components/howUse/HowUse.jsx
@@ -14,9 +14,9 @@ const HowUse = () => {
                     subtitle="Aprende a utilizar nuestra aplicación facilmente."
                     position="center"
                 />
-                <div className=" tw-text-gray-800 tw-h-[65vh]">
+                <div className=" tw-text-gray-800 tw-h-[100vh] md:tw-h-[80vh] 2xl:tw-h-[65vh]">
                     {videoData.length !== 0 ? videoData.map((item) => (
-                    <div key={item.id} className="tw-flex tw-justify-center tw-mx-80">
+                    <div key={item.id} className="tw-flex tw-justify-center tw-mx-96 xl:tw-mx-80">
                         <article className='tw-flex tw-flex-wrap tw-justify-center tw-items-center tw-gap-2 lg:tw-h-48 tw-mb-2 2xl:tw-mb-4 tw-mt-6 tw-cursor-pointer hover:tw-text-secondary tw-duration-200'>
                                 <a href={item.href} target="_blank" rel="noreferrer" alt="" className="tw-w-full tw-object-cover tw-object-right tw-h-56 xl:tw-h-80 tw-rounded-xl hover:tw-scale-105 tw-duration-500">
                                     {/* <img src={item.imgCard} alt="" loading="lazy" className="tw-object-cover tw-object-right tw-w-full tw-h-56 xl:tw-h-80 tw-rounded-xl hover:tw-scale-105 tw-duration-500" /> */}
@@ -31,7 +31,7 @@ const HowUse = () => {
                                     allowfullscreen>
                                     </iframe>
                              
-                                <div className="tw-block tw-mt-10 xl:tw-mt-2">
+                                <div className="tw-block tw-mt-4 xl:tw-mt-2">
                                     <small className="tw-mt-2 lg:tw-mt-4 tw-text-secondary">{item.subheading}</small>
                                     <h5 className='tw-opacity-100 2xl:tw-font-bold tw-text-base lg:tw-text-xl 2xl:tw-text-2xl'>{item?.headings}</h5>
                                     <p className=''>{item?.texts}</p>
