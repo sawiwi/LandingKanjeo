664502a5d4dd32e3eb1ec96ffe2d84ed75e58929
Fabian Salas <fabian.salas.astete@gmail.com>
2024-08-11 03:40:52 -0400

    fix sizes of responsive

--- src/components/portalUsers/profileRealtor/detailsRealtor/DetailPropRealtor.jsx
+++ src/components/portalUsers/profileRealtor/detailsRealtor/DetailPropRealtor.jsx
@@ -71,7 +71,7 @@ const DetailCantProps = ({onClose, data}) => {
                 <img
                   src={NotFoundProp}
                   alt="img-casa-not-found"
-                  className="tw-h-40 xl:tw-h-36 tw-w-full tw-object-scale-down group-hover:-tw-translate-y-2 tw-duration-200 tw-p-4 2xl:tw-mx-36"
+                  className="tw-h-40 xl:tw-h-36 tw-w-full tw-object-scale-down group-hover:-tw-translate-y-2 tw-duration-200 tw-p-4 2xl:tw-mx-2"
                 />
                 <small className="tw-absolute tw-top-1 tw-left-1 tw-p-[0.15rem] tw-px-4 tw-font-normal tw-opacity-100 group-hover:tw-opacity-70 tw-bg-secondary tw-text-gray-50 tw-rounded-sm group-hover:tw-top-0 tw-duration-200">
                   {property?.typeOfPropertyId || ''}
@@ -90,7 +90,7 @@ const DetailCantProps = ({onClose, data}) => {
           ))
         ) : (
           <div className="tw-shadow-lg tw-flex tw-flex-row tw-border-2 tw-h-full md:tw-h-60 xl:tw-h-full 2xl:tw-h-full md:tw-w-full tw-p-2  xl:tw-overflow-hidden 2xl:tw-p-1">
-            <div className="tw-mx-36 tw-text-center">
+            <div className="tw-mx-36 2xl:tw-mx-10 tw-text-center">
               No cuenta con propiedades activas
             </div>
           </div>
--- src/services/ExchangeRateServices.js
+++ src/services/ExchangeRateServices.js
@@ -5,11 +5,18 @@ const apikey = process.env.REACT_APP_API_URL_EXCHANGE_RATE_UF_API_KEY;
 
 const ExchangeRateServices = {
   getExchangeRateUF: async () => {
-    const response = await axios.get(
-      // `https://api.cmfchile.cl/api-sbifv3/recursos_api/uf?apikey=b3c682f4e4e29811fa1fd8d3781a463b59181fb7&formato=json`
-      `https://api.cmfchile.cl/api-sbifv3/recursos_api/uf?apikey=${apikey}&formato=json`
-    );
-    return response.data;
+    try {
+      const response = await axios.get(
+        // `https://api.cmfchile.cl/api-sbifv3/recursos_api/uf?apikey=b3c682f4e4e29811fa1fd8d3781a463b59181fb7&formato=json`
+        `https://api.cmfchile.cl/api-sbifv3/recursos_api/uf?apikey=${apikey}&formato=json`
+      );
+      return response.data;
+
+    }catch (error) {
+      console.log('error al conseguir valores de cmf', error);
+      throw error
+    }
+
   },
 };
 
